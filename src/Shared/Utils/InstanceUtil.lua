6334ff7833d87b00db538df155aee9cecc9b0f25
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-09-21 11:03:29 +0100

    + Luau and logic fixes in Remotes

---
+++ src/Shared/Utils/InstanceUtil.lua
@@ -0,0 +1,11 @@
+local InstanceUtil = {}
+
+-- Wrapper for creating an instance and defining some basic property values
+function InstanceUtil.new(className, name: string, parent: Instance)
+    local instance = Instance.new(className)
+    instance.Name = name
+    instance.Parent = parent
+    return instance
+end
+
+return InstanceUtil
