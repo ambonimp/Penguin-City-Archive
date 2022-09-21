4322c29f9dd46df0478dbf5193b7b8720c0b9ffa
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-09-21 09:42:41 +0100

    Resolve conflicts with new packages paths

--- src/Shared/Remotes.lua
+++ src/Shared/Remotes.lua
@@ -1,11 +1,9 @@
+local Remotes = {}
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
-
-local Remotes = {}
-
-local Promise = require(script.Parent.Promise)
-
+local Promise = require(ReplicatedStorage.Packages.promise)
 
 local eventHandlers = {}
 local functionHandlers = {}
@@ -17,226 +15,209 @@ local communication, functionFolder, eventFolder
 
 -- Unessecary, i just like one liners
 local function instance(class, name, parent)
-    local creating = Instance.new(class)
-    creating.Name = name
-    creating.Parent = parent
+	local creating = Instance.new(class)
+	creating.Name = name
+	creating.Parent = parent
 
-    return creating
+	return creating
 end
 
 local function waitForChild(parent, awaiting)
-    local returning = parent:FindFirstChild(awaiting)
+	local returning = parent:FindFirstChild(awaiting)
 
-    if not returning then
-        Promise.fromEvent(parent.ChildAdded, function(added)
-            if added.Name == awaiting then
-                returning = added
-                return true
-            end
-            return false
-        end)
-        :await()
-    end
+	if not returning then
+		Promise.fromEvent(parent.ChildAdded, function(added)
+			if added.Name == awaiting then
+				returning = added
+				return true
+			end
+			return false
+		end):await()
+	end
 
-    return returning
+	return returning
 end
 
-
 local function getFunctionHandler(name)
-    assert(typeof(name) == "string", "Remote name is not a string -> " .. name)
+	assert(typeof(name) == "string", "Remote name is not a string -> " .. name)
 
-    local handler = functionHandlers[name]
-    if handler then return handler end
+	local handler = functionHandlers[name]
+	if handler then
+		return handler
+	end
 
-    handler = {}
-    handler.Remote = isServer and instance("RemoteFunction", tostring(name), functionFolder) or waitForChild(functionFolder, name)
-    if isStudio and not isServer then -- anti hack
-        handler.Remote.Name = "NO WAY JOSE"
-    end
+	handler = {}
+	handler.Remote = isServer and instance("RemoteFunction", tostring(name), functionFolder) or waitForChild(functionFolder, name)
+	if isStudio and not isServer then -- anti hack
+		handler.Remote.Name = "NO WAY JOSE"
+	end
 
+	local set = false
+	function handler.registerCallback(callback)
+		if set then
+			error(string.format("Attempt to overwrite callback: %s", name))
+		end
 
-    local set = false
-    function handler.registerCallback(callback)
-        if set then
-            error(string.format("Attempt to overwrite callback: %s", name))
-        end
+		set = true
+		handler.Remote[isServer and "OnServerInvoke" or "OnClientInvoke"] = callback
+	end
 
-        set = true
-        handler.Remote[isServer and "OnServerInvoke" or "OnClientInvoke"] = callback
-
-    end
-
-
-    functionHandlers[name] = handler
-    return handler
-
+	functionHandlers[name] = handler
+	return handler
 end
 
 local function getEventHandler(name)
-    assert(typeof(name) == "string", "Remote name is not a string -> " .. name)
+	assert(typeof(name) == "string", "Remote name is not a string -> " .. name)
 
-    local handler = eventHandlers[name]
-    if handler then return handler end
+	local handler = eventHandlers[name]
+	if handler then
+		return handler
+	end
 
-    handler = {}
-    handler.Remote = isServer and instance("RemoteEvent", name, eventFolder) or waitForChild(eventFolder, name)
-    if isStudio and not isServer then -- anti hack
-        handler.Remote.Name = "YOUR MOM"
-    end
+	handler = {}
+	handler.Remote = isServer and instance("RemoteEvent", name, eventFolder) or waitForChild(eventFolder, name)
+	if isStudio and not isServer then -- anti hack
+		handler.Remote.Name = "YOUR MOM"
+	end
 
-    local callbacks = {}
-    local history = {}
+	local callbacks = {}
+	local history = {}
 
-    function handler.registerCallback(callback, temp)
-        if not temp then
-            for _, fire in ipairs(history) do
-                task.spawn(callback, table.unpack(fire))
-            end
-        end
+	function handler.registerCallback(callback, temp)
+		if not temp then
+			for _, fire in ipairs(history) do
+				task.spawn(callback, table.unpack(fire))
+			end
+		end
 
-        local i = #callbacks + 1
-        callbacks[i] = callback
+		local i = #callbacks + 1
+		callbacks[i] = callback
 
-        return function()
-            table.remove(callbacks, i)
-        end
+		return function()
+			table.remove(callbacks, i)
+		end
+	end
 
-    end
+	handler.Remote[isServer and "OnServerEvent" or "OnClientEvent"]:Connect(function(...)
+		table.insert(history, table.pack(...))
+		for _, callback in ipairs(callbacks) do
+			task.spawn(callback, ...)
+		end
+	end)
 
-    handler.Remote[isServer and "OnServerEvent" or "OnClientEvent"]:Connect(function(...)
-        table.insert(history, table.pack(...))
-        for _, callback in ipairs(callbacks) do
-            task.spawn(callback, ...)
-        end
-    end)
-
-    eventHandlers[name] = handler
-    return handler
-
+	eventHandlers[name] = handler
+	return handler
 end
 
 -- Bindings, pass a dictionary of remotes to create / connect to
 function Remotes.bindFunctions(callbacks)
-    for name, callback in pairs(callbacks) do
-        assert(callback, name)
-        assert(typeof(callback) == "function", name)
+	for name, callback in pairs(callbacks) do
+		assert(callback, name)
+		assert(typeof(callback) == "function", name)
 
-        task.spawn(function()
-            local handler = getFunctionHandler(name)
-            handler.registerCallback(callback)
-
-        end)
-    end
-
+		task.spawn(function()
+			local handler = getFunctionHandler(name)
+			handler.registerCallback(callback)
+		end)
+	end
 end
 
 function Remotes.bindEvents(callbacks)
-    for name, callback in pairs(callbacks) do
-        assert(callback, name)
-        assert(typeof(callback) == "function", name)
+	for name, callback in pairs(callbacks) do
+		assert(callback, name)
+		assert(typeof(callback) == "function", name)
 
-        task.spawn(function()
-            local handler = getEventHandler(name)
-            handler.registerCallback(callback)
-        end)
-
-    end
-
+		task.spawn(function()
+			local handler = getEventHandler(name)
+			handler.registerCallback(callback)
+		end)
+	end
 end
 
 function Remotes.bindEventTemp(name, callback) -- SYNCROHOUNOUS
-    local handler = getEventHandler(name)
-    local disconnect = handler.registerCallback(callback, true)
+	local handler = getEventHandler(name)
+	local disconnect = handler.registerCallback(callback, true)
 
-    local returning = {}
-    returning.Disconnect = disconnect
-    returning.Destroy = disconnect
+	local returning = {}
+	returning.Disconnect = disconnect
+	returning.Destroy = disconnect
 
-    return returning
-
+	return returning
 end
 
-
-
 if isServer then
-    communication = instance("Folder", "Communication", ReplicatedStorage)
-    functionFolder = instance("Folder", "Functions", communication)
-    eventFolder = instance("Folder", "Events", communication)
+	communication = instance("Folder", "Communication", ReplicatedStorage)
+	functionFolder = instance("Folder", "Functions", communication)
+	eventFolder = instance("Folder", "Events", communication)
 
+	function Remotes.invokeClient(client, name, ...)
+		assert(client.Parent == Players, "Can't fire to non-existent player " .. client.Name)
 
-    function Remotes.invokeClient(client, name, ...)
-        assert(client.Parent == Players, "Can't fire to non-existent player " .. client.Name)
+		local handler = getFunctionHandler(name)
+		local remote = assert(handler.Remote, name)
 
-        local handler = getFunctionHandler(name)
-        local remote = assert(handler.Remote, name)
+		return remote:InvokeClient(client, ...)
+	end
 
-        return remote:InvokeClient(client, ...)
-    end
+	function Remotes.invokeClients(clients, ...)
+		local returning = {}
+		for _, player in ipairs(clients) do
+			returning[player] = table.pack(Remotes.invokeClient(player, ...))
+		end
+		return returning
+	end
 
-    function Remotes.invokeClients(clients, ...)
-        local returning = {}
-        for _, player in ipairs(clients) do
-            returning[player] = table.pack(Remotes.invokeClient(player, ...))
-        end
-        return returning
-    end
+	function Remotes.invokeAllClients(...)
+		return Remotes.invokeClients(Players:GetPlayers(), ...)
+	end
 
-    function Remotes.invokeAllClients(...)
-        return Remotes.invokeClients(Players:GetPlayers(), ...)
-    end
+	function Remotes.fireClient(client, name, ...)
+		task.spawn(function(...)
+			assert(client.Parent == Players, "Can't fire to non-existent player " .. client.Name)
 
-    function Remotes.fireClient(client, name, ...)
-        task.spawn(function(...)
-            assert(client.Parent == Players, "Can't fire to non-existent player " .. client.Name)
+			local handler = getEventHandler(name)
+			local remote = assert(handler.Remote, name)
+			remote:FireClient(client, ...)
+		end, ...)
+	end
 
-            local handler = getEventHandler(name)
-            local remote = assert(handler.Remote, name)
-            remote:FireClient(client, ...)
+	function Remotes.fireClients(clients, ...)
+		for _, player in ipairs(clients) do
+			Remotes.fireClient(player, ...)
+		end
+	end
 
-        end, ...)
+	function Remotes.fireAllClients(...)
+		Remotes.fireClients(Players:GetPlayers(), ...)
+	end
 
-    end
-
-    function Remotes.fireClients(clients, ...)
-        for _, player in ipairs(clients) do
-            Remotes.fireClient(player, ...)
-        end
-    end
-
-    function Remotes.fireAllClients(...)
-        Remotes.fireClients(Players:GetPlayers(), ...)
-    end
-
-    function Remotes.fireAllOtherClients(ignore, ...)
-        for _, player in ipairs(Players:GetPlayers()) do
-            if player ~= ignore then
-                Remotes.fireClient(player, ...)
-            end
-        end
-    end
-
+	function Remotes.fireAllOtherClients(ignore, ...)
+		for _, player in ipairs(Players:GetPlayers()) do
+			if player ~= ignore then
+				Remotes.fireClient(player, ...)
+			end
+		end
+	end
 else
-    communication = ReplicatedStorage:WaitForChild("Communication")
-    functionFolder = communication:WaitForChild("Functions")
-    eventFolder = communication:WaitForChild("Events")
+	communication = ReplicatedStorage:WaitForChild("Communication")
+	functionFolder = communication:WaitForChild("Functions")
+	eventFolder = communication:WaitForChild("Events")
 
+	function Remotes.fireServer(name, ...)
+		task.spawn(function(...)
+			local handler = getEventHandler(name)
+			local remote = assert(handler.Remote, name)
 
-    function Remotes.fireServer(name, ...)
-        task.spawn(function(...)
-            local handler = getEventHandler(name)
-            local remote = assert(handler.Remote, name)
+			remote:FireServer(...)
+		end, ...)
+	end
 
-            remote:FireServer(...)
-        end, ...)
-    end
+	function Remotes.invokeServer(name, ...)
+		local handler = getFunctionHandler(name)
+		local remote = assert(handler.Remote, name)
 
-    function Remotes.invokeServer(name, ...)
-        local handler = getFunctionHandler(name)
-        local remote = assert(handler.Remote, name)
-
-        return remote:InvokeServer(...)
-    end
-
+		return remote:InvokeServer(...)
+	end
 end
 
 return Remotes
