9d62c06df83ffd5899edcf2c5edd1aa047125ff6
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-09-21 12:53:14 +0100

    Add Cmdr

--- src/Server/Paths/init.lua
+++ src/Server/Paths/init.lua
@@ -8,6 +8,36 @@ local Constants = Shared.Constants
 Paths.Modules = {}
 Paths.Initialized = false
 
+-- Intellisense
+if false then
+    -- Constants
+    Paths.Modules["GameConstants"] = require(Constants.GameConstants)
+    Paths.Modules["VehicleConstants"] = require(Constants.VehicleConstants)
+    Paths.Modules["FrameworkConstants"] = require(Constants.FrameworkConstants)
+
+    -- Packages
+    Paths.Modules["Promise"] = require(Packages.promise)
+    Paths.Modules["Maid"] = require(Packages.maid)
+    Paths.Modules["Cmdr"] = require(Packages.cmdr)
+
+    -- Shared
+    Paths.Modules["Remotes"] = require(Shared.Remotes)
+    Paths.Modules["Signal"] = require(Shared.Signal)
+
+    -- Utils
+    Paths.Modules["TableUtil"] = require(Shared.Utils.TableUtil)
+    Paths.Modules["DataUtil"] = require(Shared.Utils.DataUtil)
+    Paths.Modules["InteractionUtil"] = require(Shared.Utils.InteractionUtil)
+    Paths.Modules["VehicleUtil"] = require(Shared.Utils.VehicleUtil)
+    Paths.Modules["CmdrUtil"] = require(Shared.Utils.CmdrUtil)
+
+    --
+    Paths.Modules["PlayerData"] = require(script.PlayerData)
+    Paths.Modules["AnalyticsTracking"] = require(script.AnalyticsTracking)
+    Paths.Modules["PlayerLoader"] = require(script.PlayerLoader)
+    Paths.Modules["Vehicles"] = require(script.Vehicles)
+end
+
 function Paths.initialize()
     -- Init Modules
     local ping = tick()
@@ -30,12 +60,14 @@ function Paths.initialize()
         Paths.Modules["DataUtil"] = require(Shared.Utils.DataUtil)
         Paths.Modules["InteractionUtil"] = require(Shared.Utils.InteractionUtil)
         Paths.Modules["VehicleUtil"] = require(Shared.Utils.VehicleUtil)
+        Paths.Modules["CmdrUtil"] = require(Shared.Utils.CmdrUtil)
 
         --
         Paths.Modules["PlayerData"] = require(script.PlayerData)
         Paths.Modules["AnalyticsTracking"] = require(script.AnalyticsTracking)
         Paths.Modules["PlayerLoader"] = require(script.PlayerLoader)
         Paths.Modules["Vehicles"] = require(script.Vehicles)
+        Paths.Modules["CmdrService"] = require(script.Cmdr.CmdrService)
     end
 
     local pong = tick()
---
+++ src/Shared/Utils/CmdrUtil.lua
@@ -0,0 +1,8 @@
+local CmdrUtil = {}
+
+function CmdrUtil.IsAdmin(player: Player)
+    warn("todo CmdrUtil.IsAdmin")
+    return true
+end
+
+return CmdrUtil
