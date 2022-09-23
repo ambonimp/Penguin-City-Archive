84aa0d54ba6e1eaf501e601c667b345474d3fb88
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-09-23 09:38:31 +0100

    Develop system for choosing toppings/sauces/bases based on game progress

--- src/Shared/Minigames/Pizza/PizzaMinigameConstants.lua
+++ src/Shared/Minigames/Pizza/PizzaMinigameConstants.lua
@@ -38,30 +38,60 @@ PizzaMinigameConstants.Ingredients = {
     },
 }
 
-local toppingsWeightEquations: { [string]: WeightEquation } = {}
-local saucesWeightEquations: { [string]: WeightEquation } = {}
-local basesWeightEquations: { [string]: WeightEquation } = {}
 PizzaMinigameConstants.IngredientWeightEquations = {
-    Toppings = toppingsWeightEquations,
-    Sauces = saucesWeightEquations,
-    Bases = basesWeightEquations,
+    Toppings = {
+        --[[
+            We graph toppings' weights over time. We want each topping to have its "reign", so you feel yourself progressing through
+            the game as one of the toppings is most frequented
+            https://www.desmos.com/calculator/83yw1wfnct
+        ]]
+        Seaweed = function(alpha: number)
+            return -(4 * alpha - 0.8) + 1
+        end,
+        Squid = function(alpha: number)
+            return -(4 * alpha - 1.6) + 1
+        end,
+        Anchovies = function(alpha: number)
+            return -(4 * alpha - 2.4) + 1
+        end,
+        Shrimp = function(alpha: number)
+            return -(4 * alpha - 3.2) + 1
+        end,
+    } :: { [string]: WeightEquation },
+    Sauces = {
+        --[[
+            We graph a sauces' weight over time. We aim for hotsauce to become more prevelant as the game goes on
+            https://www.desmos.com/calculator/iaqdci8seo
+        ]]
+        TomatoSauce = function(alpha: number)
+            return -0.6 * alpha + 1
+        end,
+        HotSauce = function(alpha: number)
+            return 2 * alpha - 0.5
+        end,
+    } :: { [string]: WeightEquation },
+    Bases = {
+        -- Never changes!
+        Cheese = function(_alpha: number)
+            return 1
+        end,
+    } :: { [string]: WeightEquation },
 }
 
-local recipes: { [string]: Recipe } = {
+PizzaMinigameConstants.Recipes = {
     A = { Bases = 1, Sauces = 1 },
     B = { Bases = 1, Sauces = 1, Toppings = { 5 } },
     C = { Bases = 1, Sauces = 1, Toppings = { 2, 2 } },
     D = { Bases = 1, Sauces = 1, Toppings = { 1, 1, 1, 1 } },
-}
-PizzaMinigameConstants.Recipes = recipes
+} :: { [string]: Recipe }
 
---[[
-    We graph a recipes weight over time. At any given time, some recipes will be more likely to be chosen than others.
-    In first ~11% of the game (at time of writing), only equation A gives a value greater than 0 - so only recipe A will be
-    chosen for this time period!
-    https://www.desmos.com/calculator/bns1imkyxe
-]]
-local recipeWeightEquations: { [string]: WeightEquation } = {
+PizzaMinigameConstants.RecipeWeightEquations = {
+    --[[
+        We graph a recipes weight over time. At any given time, some recipes will be more likely to be chosen than others.
+        In first ~11% of the game (at time of writing), only equation A gives a value greater than 0 - so only recipe A will be
+        chosen for this time period!
+        https://www.desmos.com/calculator/bns1imkyxe
+    ]]
     A = function(alpha: number)
         return -1.3 * alpha + 1
     end,
@@ -74,7 +104,6 @@ local recipeWeightEquations: { [string]: WeightEquation } = {
     D = function(alpha: number)
         return -(2.8 * alpha - 1.5) ^ 2 + 0.6
     end,
-}
-PizzaMinigameConstants.RecipeWeightEquations = recipeWeightEquations
+} :: { [string]: WeightEquation }
 
 return PizzaMinigameConstants
--- src/Shared/Minigames/Pizza/PizzaMinigameUtil.lua
+++ src/Shared/Minigames/Pizza/PizzaMinigameUtil.lua
@@ -3,13 +3,16 @@ local PizzaMinigameUtil = {}
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local PizzaMinigameConstants = require(script.Parent.PizzaMinigameConstants)
 local MathUtil = require(ReplicatedStorage.Modules.Utils.MathUtil)
+local TableUtil = require(ReplicatedStorage.Modules.Utils.TableUtil)
+
+local TOTAL_TOPPINGS = TableUtil.length(PizzaMinigameConstants.Ingredients.Toppings)
 
 function PizzaMinigameUtil.rollRecipe(pizzasCompleted: number)
     local alpha = pizzasCompleted / PizzaMinigameConstants.MaxPizzas
 
     local weightTable: { [string]: number } = {}
     for recipeLabel, weightEquation in pairs(PizzaMinigameConstants.RecipeWeightEquations) do
-        local weight = weightEquation(alpha)
+        local weight = math.clamp(weightEquation(alpha), 0, 1)
         weightTable[recipeLabel] = weight
     end
 
@@ -17,6 +20,54 @@ function PizzaMinigameUtil.rollRecipe(pizzasCompleted: number)
     local recipe = PizzaMinigameConstants.Recipes[selectedRecipeLabel]
 
     return recipe
+end
+
+function PizzaMinigameUtil.rollToppings(pizzasCompleted: number, toppingsNeeded: number)
+    local alpha = pizzasCompleted / PizzaMinigameConstants.MaxPizzas
+
+    -- ERROR: Too many toppings!
+    if toppingsNeeded > TOTAL_TOPPINGS then
+        error(("Requested to roll %d toppings, only %d toppings exist!"):format(toppingsNeeded, TOTAL_TOPPINGS))
+    end
+
+    local weightTable: { [string]: number } = {}
+    for topping, weightEquation in pairs(PizzaMinigameConstants.IngredientWeightEquations.Toppings) do
+        local weight = math.clamp(weightEquation(alpha), 0, 1)
+        weightTable[topping] = weight
+    end
+
+    local toppings: { string } = {}
+    for _ = 1, toppingsNeeded do
+        local selectedTopping: string = MathUtil.selectKeyFromValueWeights(weightTable)
+        table.insert(toppings, selectedTopping)
+        weightTable[selectedTopping] = nil
+    end
+
+    return toppings
+end
+
+function PizzaMinigameUtil.rollSauce(pizzasCompleted: number)
+    local alpha = pizzasCompleted / PizzaMinigameConstants.MaxPizzas
+
+    local weightTable: { [string]: number } = {}
+    for sauce, weightEquation in pairs(PizzaMinigameConstants.IngredientWeightEquations.Sauces) do
+        local weight = math.clamp(weightEquation(alpha), 0, 1)
+        weightTable[sauce] = weight
+    end
+
+    return MathUtil.selectKeyFromValueWeights(weightTable) :: string
+end
+
+function PizzaMinigameUtil.rollBase(pizzasCompleted: number)
+    local alpha = pizzasCompleted / PizzaMinigameConstants.MaxPizzas
+
+    local weightTable: { [string]: number } = {}
+    for base, weightEquation in pairs(PizzaMinigameConstants.IngredientWeightEquations.Bases) do
+        local weight = math.clamp(weightEquation(alpha), 0, 1)
+        weightTable[base] = weight
+    end
+
+    return MathUtil.selectKeyFromValueWeights(weightTable) :: string
 end
 
 return PizzaMinigameUtil
