b56da62b91dc20570775cfd89bfc9ad931687bd6
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-09-26 13:58:21 +0100

    Merge issue (Camera)

--- src/Client/Paths/Camera.lua
+++ src/Client/Paths/Camera.lua
@@ -10,33 +10,6 @@ local camera = Workspace.CurrentCamera
 local tweenableFov = TweenableValue.new("IntValue", 70, 0.4, Enum.EasingStyle.Back, Enum.EasingDirection.Out)
 tweenableFov:BindToProperty(camera, "FieldOfView")
 
-function Camera.getCamera()
-    return camera
-end
-
-function Camera.setScriptable()
-    camera.CameraType = Enum.CameraType.Scriptable
-end
-
-function Camera.setPlayerControl()
-    camera.CameraType = Enum.CameraType.Custom
-end
-
-function Camera.viewCameraModel(cameraModel: Model)
-    -- ERROR: No lens!
-    local lens: Part = cameraModel.Lens
-    if not (lens and lens:IsA("BasePart")) then
-        error(("Passed model %s is a bad camera model"):format(cameraModel:GetFullName()))
-    end
-
-    -- ERROR: Not scriptable!
-    if not camera.CameraType == Enum.CameraType.Scriptable then
-        error("Camera is not set to scriptable! Use Camera.setScriptable()")
-    end
-
-    camera.CFrame = CFrame.new(lens.Position, lens.Position + lens.CFrame.LookVector)
-end
-
 function Camera.setFov(fov: number, animationLength: number?)
     tweenableFov:Set(fov, animationLength)
 end
