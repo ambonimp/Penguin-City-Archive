7a9354f0a85503771d6f4f907e8b835159179294
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-09-28 15:08:53 +0100

    Write some more tests for assumed instances (pizza minigame)

--- src/Server/Paths/Minigames/Pizza/PizzaMinigame.spec.lua
+++ src/Server/Paths/Minigames/Pizza/PizzaMinigame.spec.lua
@@ -10,6 +10,7 @@ return function()
 
     -- Assets
     do
+        -- Each ingredient has an asset
         local assets: Folder = minigameDirectory.Assets
         for toppingName, ingredients in pairs(PizzaMinigameConstants.Ingredients) do
             for _ingredientName, ingredientName in pairs(ingredients) do
@@ -20,12 +21,39 @@ return function()
             end
         end
 
+        -- Each asset has a primary part
         for _, assetModel: Model in pairs(assets:GetChildren()) do
             if assetModel:IsA("Model") then
                 if not assetModel.PrimaryPart then
                     table.insert(issues, ("Asset %s missing PrimaryPart"):format(assetModel:GetFullName()))
                 end
             end
+        end
+
+        -- Each sauce has a particle emitter
+        for _, sauceName in pairs(PizzaMinigameConstants.Ingredients.Sauces) do
+            local asset = assets:FindFirstChild(sauceName)
+            if asset then
+                local particleEmitter = asset:FindFirstChildWhichIsA("ParticleEmitter", true)
+                if not particleEmitter then
+                    table.insert(issues, ("Asset %s needs a sauce ParticleEmitter"):format(sauceName))
+                end
+            end
+        end
+
+        -- PizzaModel
+        local pizzaAsset = assets:FindFirstChild("Pizza")
+        if pizzaAsset then
+            local ingredients = pizzaAsset:FindFirstChild("Ingredients")
+            if not (ingredients and ingredients:IsA("Model")) then
+                table.insert(issues, ("Pizza Model %s needs an `Ingredients` Model"):format(pizzaAsset:GetFullName()))
+            end
+            local sauceParts = pizzaAsset:FindFirstChild("Sauce")
+            if not (sauceParts and #sauceParts:GetChildren() > 0) then
+                table.insert(issues, ("Pizza Model %s needs a `Sauces` Folder, with sauce parts"):format(pizzaAsset:GetFullName()))
+            end
+        else
+            table.insert(issues, ("No Pizza model in %s"):format(assets:GetFullName()))
         end
     end
 
