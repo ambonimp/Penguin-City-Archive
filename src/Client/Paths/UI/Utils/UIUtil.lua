b7c2b0846a76c048a39765e0d7eb28cc87d8e9cf
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-09-29 22:32:43 +0100

    UIUtil addition (account for anchorPoint with offsetGuiInset)

--- src/Client/Paths/UI/Utils/UIUtil.lua
+++ src/Client/Paths/UI/Utils/UIUtil.lua
@@ -2,9 +2,13 @@ local UIUtil = {}
 
 local GuiService = game:GetService("GuiService")
 
+--[[
+    If we design a button to be at the top of the screen, it will still be so when we load in (even with the "roblox top bar")
+    - Takes into account the AnchorPoint of the object (i.e., GuiObject centered on the screen will remain as such)
+]]
 function UIUtil.offsetGuiInset(guiObject: GuiObject)
     local guiInset = GuiService:GetGuiInset()
-    local guiInsetUDim2 = UDim2.new(0, guiInset.X, 0, guiInset.Y)
+    local guiInsetUDim2 = UDim2.new(0, guiInset.X, 0, guiInset.Y * guiObject.AnchorPoint.Y)
     guiObject.Position -= guiInsetUDim2
 end
 
