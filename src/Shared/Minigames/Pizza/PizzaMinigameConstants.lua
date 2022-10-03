e3ff5ce1abb03dde0e8fbf19c2c4cd61a042c839
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-10-03 11:54:58 +0100

    balance values

--- src/Shared/Minigames/Pizza/PizzaMinigameConstants.lua
+++ src/Shared/Minigames/Pizza/PizzaMinigameConstants.lua
@@ -13,9 +13,9 @@ PizzaMinigameConstants.Reward = {
 }
 
 PizzaMinigameConstants.Conveyor = {
-    Time = 8.5, -- How many seconds the pizza takes to traverse the conveyor
-    IncreaseFactor = 0.97, -- How much to decrease this time by each iteration
-    MaxIncreases = 12,
+    Time = 12, -- How many seconds the pizza takes to traverse the conveyor
+    IncreaseFactor = 0.95, -- How much to decrease this time by each iteration
+    MaxIncreases = 10,
 }
 
 PizzaMinigameConstants.MaxMistakes = 3
