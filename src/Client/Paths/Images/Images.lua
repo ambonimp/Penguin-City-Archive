5790f0d659db0133fedfe514f1dc6ec2c8d8f141
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-10-03 08:52:14 +0100

    Move ImageIds into one file for better intellisense

--- src/Client/Paths/Images/Images.lua
+++ src/Client/Paths/Images/Images.lua
@@ -6,15 +6,83 @@
 ]]
 local Images = {}
 
-local Players = game:GetService("Players")
-local Paths = require(Players.LocalPlayer.PlayerScripts.Paths)
-local ImageContexts = Paths.Client.Images.ImageContexts
-
--- Write ImageContexts
-Images.ButtonIcons = require(ImageContexts.ButtonIcons)
-Images.Icons = require(ImageContexts.Icons)
-Images.Coins = require(ImageContexts.Coins)
-Images.PizzaMinigame = require(ImageContexts.PizzaMinigame)
+--#region ButtonIcons
+Images.ButtonIcons = {
+    Family = "rbxassetid://11152372064",
+    FoldedMap = "rbxassetid://11152371848",
+    Igloo = "rbxassetid://11152371425",
+    Inventory = "rbxassetid://11152371230",
+    Map = "rbxassetid://11152371075",
+    Party = "rbxassetid://11152370136",
+    StampBook = "rbxassetid://11152369533",
+    Toolbox = "rbxassetid://11152369313",
+}
+--#endregion
+--#region Icons
+Images.Icons = {
+    Add = "rbxassetid://11152372782",
+    Badge = "rbxassetid://11152372668",
+    Bag = "rbxassetid://11152372582",
+    Book = "rbxassetid://11152372490",
+    Close = "rbxassetid://11152372397",
+    Events = "rbxassetid://11152372332",
+    Exit = "rbxassetid://11152372250",
+    Face = "rbxassetid://11152372168",
+    FloorCarpet = "rbxassetid://11152371963",
+    Furniture = "rbxassetid://11152371773",
+    Hat = "rbxassetid://11152371685",
+    Igloo = "rbxassetid://11152371572",
+    Instructions = "rbxassetid://11152371331",
+    Layout = "rbxassetid://11152371133",
+    Message = "rbxassetid://11152371010",
+    Minigame = "rbxassetid://11152370898",
+    Move = "rbxassetid://11152370757",
+    Okay = "rbxassetid://11152370624",
+    Outfit = "rbxassetid://11152370509",
+    Owner = "rbxassetid://11152370384",
+    Paint = "rbxassetid://11152370281",
+    PaintBucket = "rbxassetid://11152373133",
+    Pants = "rbxassetid://11152370202",
+    Pets = "rbxassetid://11152370072",
+    Place = "rbxassetid://11152370003",
+    Rotate = "rbxassetid://11152369890",
+    Seal = "rbxassetid://11152369785",
+    Search = "rbxassetid://11152369702",
+    Shirt = "rbxassetid://11152369627",
+    Text = "rbxassetid://11152369398",
+    Wallpaper = "rbxassetid://11152369233",
+    WindowDoor = "rbxassetid://11152369143",
+    Wrench = "rbxassetid://11152369091",
+}
+--#endregion
+--#region Coins
+Images.Coins = {
+    Bundle1 = "rbxassetid://11152356128",
+    Bundle2 = "rbxassetid://11152356045",
+    Bundle3 = "rbxassetid://11152355987",
+    Bundle4 = "rbxassetid://11152355907",
+    Bundle5 = "rbxassetid://11152355811",
+    Bundle6 = "rbxassetid://11152355721",
+}
+--#endregion
+--#region PizzaMinigame
+Images.PizzaMinigame = {
+    Squid = "rbxassetid://11152398223",
+    HotSauce = "rbxassetid://11152398287",
+    Shrimp = "rbxassetid://11152372064",
+    Seaweed = "rbxassetid://11152398463",
+    Sauce = "rbxassetid://11152398581",
+    Logo = "rbxassetid://11152398785",
+    LogoCartoon = "rbxassetid://11152398954",
+    InstructionsPaper = "rbxassetid://11152399041",
+    Doodle1 = "rbxassetid://11152399315",
+    Doodle2 = "rbxassetid://11152399219",
+    Doodle3 = "rbxassetid://11152399111",
+    Crust = "rbxassetid://11152399418",
+    Cheese = "rbxassetid://11152399496",
+    Anchoves = "rbxassetid://11152399621",
+}
+--#endregion
 
 --!! ImageViewer assumes all keys of Images are a table of ImageIds
 
