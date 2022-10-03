1068a8110e45b212246635d94ac3992b80607275
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-10-03 07:33:51 +0100

    KeyboardButton + UIUtil tweaks

--- src/Client/Paths/UI/Utils/UIUtil.lua
+++ src/Client/Paths/UI/Utils/UIUtil.lua
@@ -8,7 +8,7 @@ local GuiService = game:GetService("GuiService")
 ]]
 function UIUtil.offsetGuiInset(guiObject: GuiObject)
     local guiInset = GuiService:GetGuiInset()
-    local guiInsetUDim2 = UDim2.new(0, guiInset.X, 0, guiInset.Y * guiObject.AnchorPoint.Y)
+    local guiInsetUDim2 = UDim2.new(0, guiInset.X, 0, (1 - guiInset.Y) * guiObject.AnchorPoint.Y)
     guiObject.Position -= guiInsetUDim2
 end
 
