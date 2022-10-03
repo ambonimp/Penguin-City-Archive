3c83eec3199d11f182dd82d0a3f2a03aad4d8abe
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-10-03 17:03:56 +0100

    Better cursordown/up support for mobile touch

--- src/Client/Paths/Input/InputController.lua
+++ src/Client/Paths/Input/InputController.lua
@@ -5,6 +5,7 @@ local UserInputService = game:GetService("UserInputService")
 local Paths = require(Players.LocalPlayer.PlayerScripts.Paths)
 local Signal = require(Paths.Shared.Signal)
 local InputConstants = require(Paths.Client.Input.InputConstants)
+local DeviceUtil = require(Paths.Client.Utils.DeviceUtil)
 
 -- Generic Events that are compatible across PC/Mobile/XBox
 InputController.CursorDown = Signal.new()
@@ -13,8 +14,8 @@ InputController.CursorUp = Signal.new()
 -- Listen to Input
 do
     UserInputService.InputBegan:Connect(function(inputObject, gameProcessedEvent)
-        -- RETURN: gameProcessedEvent
-        if gameProcessedEvent then
+        -- RETURN: gameProcessedEvent  (minus mobile, finicky!)
+        if gameProcessedEvent and inputObject.UserInputType ~= Enum.UserInputType.Touch then
             return
         end
 
@@ -23,12 +24,17 @@ do
             or table.find(InputConstants.Cursor.Down.UserInputTypes, inputObject.UserInputType) and true
             or false
         if isCursorDownInput then
+            -- Wait a frame on mobile; needs a frame to update the MouseLocation in UserInputService. This helps fix edge case bugs on this event
+            if DeviceUtil.isMobile() then
+                task.wait()
+            end
+
             InputController.CursorDown:Fire()
         end
     end)
     UserInputService.InputEnded:Connect(function(inputObject, gameProcessedEvent)
-        -- RETURN: gameProcessedEvent
-        if gameProcessedEvent then
+        -- RETURN: gameProcessedEvent (minus mobile, finicky!)
+        if gameProcessedEvent and inputObject.UserInputType ~= Enum.UserInputType.Touch then
             return
         end
 
