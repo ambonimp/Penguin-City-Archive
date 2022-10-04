59ea23ecdabdcb52a330333543fc501a2d92f388
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-10-04 10:23:37 +0100

    allow mobile/xbox to access cmdr

--- src/Shared/Utils/GameUtil.lua
+++ src/Shared/Utils/GameUtil.lua
@@ -4,6 +4,8 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local GameConstants = require(ReplicatedStorage.Shared.Constants.GameConstants)
 
+type PlaceName = "Dev" | "Live" | "QA" | "feature"
+
 function GameUtil.getPlaceId()
     return game.PlaceId
 end
@@ -22,6 +24,10 @@ end
 
 function GameUtil.isDevGame()
     return GameUtil.getGameId() == GameConstants.GameId.Dev
+end
+
+function GameUtil.getPlaceName(): PlaceName
+    return GameUtil.isLiveGame() and "Live" or GameUtil.isDevGame() and "Dev" or GameUtil.isQAGame() and "QA" or "feature"
 end
 
 return GameUtil
