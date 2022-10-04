4d63cfdfd7048a62c13da9fdb74a8697beab2287
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-10-04 10:07:13 +0100

    add dev/"Live"/qa game/place ids

--- src/Shared/Constants/GameConstants.lua
+++ src/Shared/Constants/GameConstants.lua
@@ -4,4 +4,15 @@ GameConstants.Version = "v0"
 GameConstants.GameName = "penguin-city"
 GameConstants.PrettyGameName = "Penguin City"
 
+GameConstants.PlaceId = {
+    Live = 1337,
+    QA = 9118461324,
+    Dev = 10787436070,
+}
+GameConstants.GameId = {
+    Live = 1337,
+    QA = 3425594443,
+    Dev = 3899496745,
+}
+
 return GameConstants
