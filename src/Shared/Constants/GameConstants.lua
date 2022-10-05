5b047c97ffb97c1711cbdb329e3be9dd3d376435
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-10-05 07:32:11 +0100

    update gameconstants for live game + add example products

--- src/Shared/Constants/GameConstants.lua
+++ src/Shared/Constants/GameConstants.lua
@@ -6,12 +6,12 @@ GameConstants.GameName = "penguin-city"
 GameConstants.PrettyGameName = "Penguin City"
 
 GameConstants.PlaceId = {
-    Live = 1337,
+    Live = 11173876995,
     QA = 9118461324,
     Dev = 10787436070,
 }
 GameConstants.GameId = {
-    Live = 1337,
+    Live = 3992926863,
     QA = 3425594443,
     Dev = 3899496745,
 }
--- src/Shared/Products/Products.lua
+++ src/Shared/Products/Products.lua
@@ -20,19 +20,60 @@ export type Product = {
 }
 
 Products.Products = {
+    --#region Coins
     Coins = {
         {
             Id = "coin_bundle_1",
             DisplayName = "Coin Bundle 1",
             IsConsumable = true,
-            CoinData = {
-                Cost = 10,
+            RobuxData = {
+                DeveloperProductId = 1322070855,
             },
             Metadata = {
                 AddCoins = 20,
             },
         },
     } :: { Product },
+    --#endregion
+    --#region Examples (delete before release)
+    GoodExamples = {
+        -- Purchasable/promptable only once - and when owned, will print `JoinMessage` when a player who owns this Product joins the game
+        OneTimeDevProduct = {
+            Id = "one_time_dev_product",
+            DisplayName = "One Time Dev Product",
+            IsConsumable = false,
+            RobuxData = {
+                DeveloperProductId = 1337,
+            },
+            Metadata = {
+                JoinMessage = "%s is Azor Ahai",
+            },
+        },
+        -- A one-time bundle that will unlock X items for a player. We can pass a `Robux` cost, and it will use a generic DeveloperProduct to process the purchase
+        SomeBundle = {
+            Id = "some_bundle",
+            DisplayName = "Some Bundle",
+            IsConsumable = false,
+            RobuxData = {
+                Cost = 50,
+            },
+            Metadata = {
+                BundleId = 1,
+            },
+        },
+    } :: { Product },
+    BadExamples = {
+        -- BAD! Ownership of a gamepass is immutable, we should not have a representative Product like this
+        ConsumableGamepass = {
+            Id = "consumable_gamepass",
+            DisplayName = "Consumable Gamepass",
+            IsConsumable = true,
+            RobuxData = {
+                GamepassId = 1337 + 10,
+            },
+        },
+    },
+    --#endregion
 }
 
 return Products
