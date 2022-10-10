0a5a8f4f96bd37986574bf86af0c48c404953f32
Amani Mbonimpa <Amani.J.Mbonimpa@gmail.com>
2022-10-10 13:53:22 -0400

    Add support for horizontal scrolling to Carousel

--- src/Client/Paths/UI/Elements/HorizontalCarousel.story.lua
+++ src/Client/Paths/UI/Elements/HorizontalCarousel.story.lua
@@ -5,10 +5,10 @@ return function(target)
     frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
     frame.Position = UDim2.fromScale(0.5, 0.5)
     frame.AnchorPoint = Vector2.new(0.5, 0.5)
-    frame.Size = UDim2.new(0, 100, 1)
+    frame.Size = UDim2.new(0, 700, 0, 100)
     frame.Parent = target
 
-    local carousel = Carousel.new("Y")
+    local carousel = Carousel.new("X")
     carousel:SetNavigatorSize(30)
 
     for i = 1, 15 do
@@ -19,8 +19,8 @@ return function(target)
         child.Name = name
         child.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
         child.Size = UDim2.fromScale(1, 1)
+        child.SizeConstraint = Enum.SizeConstraint.RelativeYY
         child.Text = name
-        child.SizeConstraint = Enum.SizeConstraint.RelativeXX
 
         carousel:MountChild(child)
     end
