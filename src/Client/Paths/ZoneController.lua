8aa06580878f1f443d3ae08c23b5d49a7bbf6a2b
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-10-24 09:24:37 +0100

    "character reset" teleport to new zone

--- src/Client/Paths/ZoneController.lua
+++ src/Client/Paths/ZoneController.lua
@@ -14,12 +14,15 @@ local Transitions = require(Paths.Client.UI.Screens.SpecialEffects.Transitions)
 local CharacterUtil = require(Paths.Shared.Utils.CharacterUtil)
 local BooleanUtil = require(Paths.Shared.Utils.BooleanUtil)
 local MinigameController: typeof(require(Paths.Client.Minigames.MinigameController))
+local Limiter = require(Paths.Shared.Limiter)
 
 local MAX_YIELD_TIME_ZONE_LOADING = 10
 local WAIT_FOR_ZONE_TO_LOAD_INTERMISSION = 1 -- How often to verify if all base parts are loaded
+local DEFAULT_ZONE_TELEPORT_DEBOUNCE = 5
 
 local localPlayer = Players.LocalPlayer
-local currentZone = ZoneUtil.zone(ZoneConstants.ZoneType.Room, ZoneConstants.DefaultPlayerZoneState.RoomId)
+local defaultZone = ZoneUtil.zone(ZoneConstants.ZoneType.Room, ZoneConstants.DefaultPlayerZoneState.RoomId)
+local currentZone = defaultZone
 local currentRoomZone = currentZone
 local zoneMaid = Maid.new()
 local isRunningTeleportToRoomRequest = false
@@ -219,6 +222,15 @@ function ZoneController.teleportToRoomRequest(roomZone: ZoneConstants.Zone)
     end)
 
     return requestAssume
+end
+
+function ZoneController.teleportToDefaultZone()
+    -- RETURN: Debounce
+    if not Limiter.debounce("ZoneController", "DefaultZoneTeleport", DEFAULT_ZONE_TELEPORT_DEBOUNCE) then
+        return
+    end
+
+    ZoneController.teleportToRoomRequest(defaultZone)
 end
 
 -------------------------------------------------------------------------------
