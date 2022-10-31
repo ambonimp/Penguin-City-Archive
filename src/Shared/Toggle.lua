57aafb622c8145e97cf6f42692c1237f88a5f813
Amani Mbonimpa <Amani.J.Mbonimpa@gmail.com>
2022-10-31 06:33:18 -0400

    Update Toggle.lua

--- src/Shared/Toggle.lua
+++ src/Shared/Toggle.lua
@@ -2,46 +2,74 @@
     Wrapper for a boolean variable that manages potentially conflicting value changes.
 
     Example:
-        local toggle = Toggle.new(true, print)
+        local toggle = Toggle.new(true)
 
-        toggle:Set(false, "a") -- "a" (conflicts with previous bool value)
-        toggle:Set(false, "b") -- ""
-        toggle:Set(true, "c") -- ""
-        toggle:Set(true, "b") -- "b"
+        toggle:Set(false, "a")
+        print(toggle:Get()) -- false
+        toggle:Set(false, "b")
+        print(toggle:Get()) -- false
+        toggle:Set(true, "a")
+        print(toggle:Get()) -- false
+        toggle:Set(true, "b")
+        print(toggle:Get()) -- true
+
 ]]
 
 local Toggle = {}
 
-function Toggle.new(initialValue: boolean, onToggled: (boolean) -> ())
+function Toggle.new(initialValue: boolean, onToggled: (value: boolean) -> ()?)
     local toggle = {}
 
     local jobs: { any } = {}
     local value = initialValue
 
-    --[[
-        Change the value, if flipping back the value to the initial value, all jobs must agree
-    ]]
+    -- Change the value, if flipping back the value to the initial value, all jobs must agree
     function toggle:Set(newValue: boolean, job: any)
         if newValue ~= initialValue then
-            if not table.find(jobs, job) then
-                if value ~= newValue then
-                    value = true
-                    onToggled(true)
-                end
-
-                table.insert(jobs, job)
+            -- RETURN: Job already exists
+            if table.find(jobs, job) then
+                return
             end
+
+            if value ~= newValue then
+                value = true
+
+                if onToggled then
+                    onToggled(newValue)
+                end
+            end
+
+            table.insert(jobs, job)
         else
             local jobIndex = table.find(jobs, job)
-            if jobIndex then
-                table.remove(jobs, jobIndex)
+            -- RETURN: Job doesn't exist
+            if not jobIndex then
+                return
+            end
 
-                if #jobs == 0 then
-                    value = false
-                    onToggled(false)
+            table.remove(jobs, jobIndex)
+
+            if #jobs == 0 then
+                value = false
+
+                if onToggled then
+                    onToggled(newValue)
                 end
             end
         end
+    end
+
+    function toggle:ForceSet(newValue)
+        if onToggled then
+            onToggled(newValue)
+        end
+
+        jobs = {}
+        value = newValue
+    end
+
+    function toggle:Get(): boolean
+        return value
     end
 
     -- Calls internal onToggled callback without alterating internal workings of Toggle
