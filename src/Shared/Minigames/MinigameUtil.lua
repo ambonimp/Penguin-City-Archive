2953bb8b36986c70089e182364e25fd1784d5106
Amani Mbonimpa <Amani.J.Mbonimpa@gmail.com>
2022-11-07 02:27:27 -0500

    Clean up minigame constants and utils

--- src/Shared/Minigames/MinigameUtil.lua
+++ src/Shared/Minigames/MinigameUtil.lua
@@ -3,7 +3,7 @@ local MinigameUtil = {}
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local MinigameConstants = require(ReplicatedStorage.Shared.Minigames.MinigameConstants)
 
-function MinigameUtil.getSessionConfigs(minigameName): MinigameConstants.SessionConfig
+function MinigameUtil.getSessionConfigs(minigameName: string): MinigameConstants.SessionConfig
     return require(ReplicatedStorage.Shared.Minigames[minigameName][minigameName .. "Constants"]).SessionConfig
 end
 
