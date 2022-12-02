8d21fa6d118e716e7052f422efcc491df3491df0
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-12-02 10:02:01 +0000

    add some temporary snowball tools for testing

--- src/Shared/Tools/Categories/SnowballToolConstants.lua
+++ src/Shared/Tools/Categories/SnowballToolConstants.lua
@@ -10,6 +10,26 @@ items.Default = {
     Name = "Default",
     Price = 0,
 }
+items.red = {
+    Name = "red",
+    Price = 1,
+}
+items.black = {
+    Name = "black",
+    Price = 1,
+}
+items.green = {
+    Name = "green",
+    Price = 1,
+}
+items.yellow = {
+    Name = "yellow",
+    Price = 1,
+}
+items.blue = {
+    Name = "blue",
+    Price = 1,
+}
 
 SnowballToolConstants.Items = items
 
