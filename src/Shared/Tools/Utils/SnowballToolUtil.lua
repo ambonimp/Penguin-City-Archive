e5fe8cda7357fe05c559ad73bacc62e418c50f35
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-12-05 12:17:15 +0000

    highlight snowball local player throws

--- src/Shared/Tools/Utils/SnowballToolUtil.lua
+++ src/Shared/Tools/Utils/SnowballToolUtil.lua
@@ -1,5 +1,7 @@
 local SnowballToolUtil = {}
 
+local COLOR_WHITE = Color3.fromRGB(255, 255, 255)
+
 function SnowballToolUtil.hideSnowball(snowballModel: Model)
     snowballModel.PrimaryPart.Transparency = 1
 end
@@ -10,6 +12,17 @@ end
 
 function SnowballToolUtil.matchSnowball(snowballModel: Model, oldSnowballModel: Model)
     snowballModel.PrimaryPart.Transparency = oldSnowballModel.PrimaryPart.Transparency
+end
+
+function SnowballToolUtil.highlight(snowballModel: Model)
+    local highlight = Instance.new("Highlight")
+    highlight.FillColor = COLOR_WHITE
+    highlight.OutlineColor = COLOR_WHITE
+    highlight.FillTransparency = 0.6
+    highlight.OutlineTransparency = 0.5
+    highlight.Parent = snowballModel.PrimaryPart
+
+    return highlight
 end
 
 return SnowballToolUtil
