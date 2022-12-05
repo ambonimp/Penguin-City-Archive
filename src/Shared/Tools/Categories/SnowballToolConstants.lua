6cbe629df0e3781319b11b52b295cf9210fc701b
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-12-05 23:30:09 +0000

    delete testing snowball tools

--- src/Shared/Tools/Categories/SnowballToolConstants.lua
+++ src/Shared/Tools/Categories/SnowballToolConstants.lua
@@ -11,31 +11,6 @@ items.Default = {
     DisplayName = "Snowball",
     Price = 0,
 }
-items.red = {
-    Id = "red",
-    DisplayName = "Red Snowball",
-    Price = 1,
-}
-items.black = {
-    Id = "black",
-    DisplayName = "Black Snowball",
-    Price = 1,
-}
-items.green = {
-    Id = "green",
-    DisplayName = "Green Snowball",
-    Price = 1,
-}
-items.yellow = {
-    Id = "yellow",
-    DisplayName = "Yellow Snowball",
-    Price = 1,
-}
-items.blue = {
-    Id = "blue",
-    DisplayName = "Blue Snowball",
-    Price = 1,
-}
 
 SnowballToolConstants.Items = items
 
