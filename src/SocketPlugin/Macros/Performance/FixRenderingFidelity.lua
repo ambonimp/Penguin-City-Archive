40800f109fee9a235693af035f70d68e339d5c96
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-12-06 09:02:42 +0000

    add fix rendering fidelity macro

---
+++ src/SocketPlugin/Macros/Performance/FixRenderingFidelity.lua
@@ -0,0 +1,47 @@
+local Workspace = game:GetService("Workspace")
+local ServerStorage = game:GetService("ServerStorage")
+local Selection = game:GetService("Selection")
+local Utils = ServerStorage.SocketPlugin:FindFirstChild("Utils")
+local Logger = require(Utils.Logger)
+
+local THROTTLE_EVERY = 1000
+
+local macroDefinition = {
+    Name = "Fix Rendering Fidelity",
+    Group = "Performance",
+    Icon = "⛰️",
+    Description = "Sets the rendering fidelity of ALL parts to be as performatic as possible",
+    EnableAutomaticUndo = false,
+    Function = function(macro, _plugin)
+        -- WARN: No selection!
+        local selection = Selection:Get()
+        if #selection == 0 then
+            Logger:MacroWarn(macro, "No selection!")
+            return
+        end
+
+        local totalUpdates = 0
+        for _, selectionInstance in pairs(selection) do
+            -- Get Instances
+            local instances = selectionInstance:GetDescendants()
+            table.insert(instances, selectionInstance)
+
+            -- Iterate
+            for i, instance: MeshPart in pairs(instances) do
+                -- Throttle
+                if i % THROTTLE_EVERY == 0 then
+                    task.wait()
+                end
+
+                if instance:IsA("MeshPart") and instance.RenderFidelity ~= Enum.RenderFidelity.Performance then
+                    totalUpdates += 1
+                    instance.RenderFidelity = Enum.RenderFidelity.Performance
+                end
+            end
+        end
+
+        Logger:MacroInfo(macro, ("Updated %d MeshParts"):format(totalUpdates))
+    end,
+}
+
+return macroDefinition
---
+++ src/SocketPlugin/Macros/Performance/Group.lua
@@ -0,0 +1,5 @@
+return {
+    Group = "Performance",
+    GroupIcon = "💨",
+    Disabled = true,
+}
