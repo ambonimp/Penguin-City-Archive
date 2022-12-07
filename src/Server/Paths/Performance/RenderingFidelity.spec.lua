08d2980384a5d85cab70f3dbea01db3a516a4f2f
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-12-07 10:53:14 +0000

    test checking for rendering fidelity

---
+++ src/Server/Paths/Performance/RenderingFidelity.spec.lua
@@ -0,0 +1,27 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TestUtil = require(ReplicatedStorage.Shared.Utils.TestUtil)
+local DescendantLooper = require(ReplicatedStorage.Shared.DescendantLooper)
+
+return function()
+    local issues: { string } = {}
+
+    local totalFixesNeeded = 0
+    DescendantLooper.workspace(function(instance)
+        return instance:IsA("MeshPart")
+    end, function(meshPart: MeshPart)
+        if meshPart.RenderFidelity ~= Enum.RenderFidelity.Performance then
+            totalFixesNeeded += 1
+        end
+    end, true)
+
+    if totalFixesNeeded > 0 then
+        table.insert(
+            issues,
+            ("%d MeshParts found with non-Performance Rendering Fidelity. Run the Fix Rendering Fidelity macro to fix this."):format(
+                totalFixesNeeded
+            )
+        )
+    end
+
+    return issues
+end
