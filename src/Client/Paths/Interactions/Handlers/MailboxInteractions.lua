135e2aad4bcae3009e5cb06a10d544c32516a74f
Luis Figueredo <luis152323@gmail.com>
2022-12-09 11:03:37 -0500

    Update Mailbox for Radial menu
    "Plot" option and "House" option

--- src/Client/Paths/Interactions/Handlers/MailboxInteractions.lua
+++ src/Client/Paths/Interactions/Handlers/MailboxInteractions.lua
@@ -8,7 +8,21 @@ local UIController = require(Paths.Client.UI.UIController)
 
 local uiStateMachine = UIController.getStateMachine()
 
-InteractionController.registerInteraction("MailboxSettings", function(instance, prompt)
+--Keep this for future use in case we need it
+-- InteractionController.registerInteraction("MailboxSettings", function(instance, prompt)
+--     local state = uiStateMachine:GetState()
+--     if
+--         not (
+--             state == UIConstants.States.PlotSettings
+--             or state == UIConstants.States.HouseSelectionUI
+--             or state == UIConstants.States.PlotChanger
+--         )
+--     then
+--         uiStateMachine:Push(UIConstants.States.PlotSettings, { Instance = instance, Prompt = prompt, PlotAt = prompt.Parent.Parent })
+--     end
+-- end)
+
+InteractionController.registerInteraction("Plot", function(instance, prompt)
     local state = uiStateMachine:GetState()
     if
         not (
@@ -17,7 +31,20 @@ InteractionController.registerInteraction("MailboxSettings", function(instance, 
             or state == UIConstants.States.PlotChanger
         )
     then
-        uiStateMachine:Push(UIConstants.States.PlotSettings, { Instance = instance, Prompt = prompt, PlotAt = prompt.Parent.Parent })
+        uiStateMachine:Push(UIConstants.States.PlotChanger, { Instance = instance, Prompt = prompt, PlotAt = prompt.Parent.Parent })
+    end
+end)
+
+InteractionController.registerInteraction("House", function(instance, prompt)
+    local state = uiStateMachine:GetState()
+    if
+        not (
+            state == UIConstants.States.PlotSettings
+            or state == UIConstants.States.HouseSelectionUI
+            or state == UIConstants.States.PlotChanger
+        )
+    then
+        uiStateMachine:Push(UIConstants.States.HouseSelectionUI, { Instance = instance, Prompt = prompt, PlotAt = prompt.Parent.Parent })
     end
 end)
 
