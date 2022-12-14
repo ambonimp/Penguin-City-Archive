ca216fe193a3f4c6ac682a5fa2cdf09a68088fea
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-12-14 18:25:27 +0000

    Implement Sentry

---
+++ src/Sentry/StartErrorTracking.server.lua
@@ -0,0 +1,6 @@
+local ServerScriptService = game:GetService("ServerScriptService")
+local Sentry = require(ServerScriptService.Sentry)
+
+Sentry.init({
+    dsn = "https://8e78d4177a7441d3ae162d8cd5c98277@o987012.ingest.sentry.io/4504328654815233",
+})
