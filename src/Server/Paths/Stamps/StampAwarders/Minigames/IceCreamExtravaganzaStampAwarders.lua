1f06f81fc77a326db95677c94de87b3f316039b2
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-12-19 10:26:01 +0000

    setup remaining ice cream stamps

--- src/Server/Paths/Stamps/StampAwarders/Minigames/IceCreamExtravaganzaStampAwarders.lua
+++ src/Server/Paths/Stamps/StampAwarders/Minigames/IceCreamExtravaganzaStampAwarders.lua
@@ -6,13 +6,14 @@ local StampService = require(Paths.Server.Stamps.StampService)
 local StampUtil = require(Paths.Shared.Stamps.StampUtil)
 local MinigameSession = require(Paths.Server.Minigames.MinigameSession)
 local MinigameConstants = require(Paths.Shared.Minigames.MinigameConstants)
+local IceCreamExtravaganzaSession = require(Paths.Server.Minigames.IceCreamExtravaganza.IceCreamExtravaganzaSession)
 
 local iceCreamExtravaganzaWinsTieredStamp = StampUtil.getStampFromId("minigame_icecream_wins")
-local playIceCreamExtravaganzaStamp = StampUtil.getStampFromId("minigame_icecream_play") --todo
-local collectScoops5Stamp = StampUtil.getStampFromId("minigame_icecream_collect5") --todo
-local collectScoops10Stamp = StampUtil.getStampFromId("minigame_icecream_collect10") --todo
+local playIceCreamExtravaganzaStamp = StampUtil.getStampFromId("minigame_icecream_play")
+local collectScoops5Stamp = StampUtil.getStampFromId("minigame_icecream_collect5")
+local collectScoops10Stamp = StampUtil.getStampFromId("minigame_icecream_collect10")
 
--- minigame_sledrace_wins
+-- minigame_icecream_wins, minigame_icecream_play
 MinigameSession.MinigameFinished:Connect(
     function(minigameSession: MinigameSession.MinigameSession, sortedScores: MinigameConstants.SortedScores)
         -- RETURN: Not ice cream extravaganza
@@ -20,9 +21,31 @@ MinigameSession.MinigameFinished:Connect(
             return
         end
 
+        -- Play
+        for _, score in pairs(sortedScores) do
+            StampService.addStamp(score.Player, playIceCreamExtravaganzaStamp.Id)
+        end
+
+        -- Wins
         local winnerPlayer = sortedScores[1] and sortedScores[1].Player
         if winnerPlayer then
             StampService.incrementStamp(winnerPlayer, iceCreamExtravaganzaWinsTieredStamp.Id, 1)
+        end
+    end
+)
+
+IceCreamExtravaganzaSession.CollectableCollected:Connect(
+    function(session: IceCreamExtravaganzaSession.IceCreamExtravaganzaSession, player: Player, _collectableType: string, _scoreIncrement: number?)
+        local currentScore = session:GetParticipantScore(player) or 0
+
+        -- 5 Scoops
+        if currentScore >= 5 then
+            StampService.addStamp(player, collectScoops5Stamp.Id)
+        end
+
+        -- 10 Scoops
+        if currentScore >= 10 then
+            StampService.addStamp(player, collectScoops10Stamp.Id)
         end
     end
 )
