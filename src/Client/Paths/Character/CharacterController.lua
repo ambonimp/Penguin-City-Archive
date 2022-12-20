d9ea60e746eebb54a2b6bc3c2a93271dbf11d2ad
Luis Figueredo <luis152323@gmail.com>
2022-12-20 10:16:31 -0500

    Mobile Sprint

--- src/Client/Paths/Character/CharacterController.lua
+++ src/Client/Paths/Character/CharacterController.lua
@@ -2,12 +2,18 @@ local CharacterController = {}
 
 local Players = game:GetService("Players")
 local Paths = require(Players.LocalPlayer.PlayerScripts.Paths)
+local PropertyStack = require(Paths.Shared.PropertyStack)
 local Loader = require(Paths.Client.Loader)
 local Maid = require(Paths.Shared.Maid)
 
 local Animate = Paths.Client.Character.Animate
 local localPlayer = Players.LocalPlayer
 local loadMaid = Maid.new()
+
+local SPRINT_WALKSPEED = 30
+local DEFAULT_WALKSPEED = 16
+
+local isSprinting = false
 
 local function unloadCharacter(_character: Model)
     loadMaid:Cleanup()
@@ -30,6 +36,15 @@ local function loadCharacter(character: Model)
             end
         end
     end)
+end
+
+function CharacterController.SetWalkspeed(new: number, key: string)
+    PropertyStack.setProperty(localPlayer.Character.Humanoid, "WalkSpeed", new, key)
+end
+
+function CharacterController.toggleSprint()
+    isSprinting = not isSprinting
+    CharacterController.SetWalkspeed(isSprinting and SPRINT_WALKSPEED or DEFAULT_WALKSPEED, "sprint")
 end
 
 Loader.giveTask("Character", "LoadCharacter", function()
---
+++ src/Client/Paths/UI/Screens/MobileButtons/MobileButtonsScreen.lua
@@ -0,0 +1,61 @@
+local MobileButtons = {}
+
+local Players = game:GetService("Players")
+local Paths = require(Players.LocalPlayer.PlayerScripts.Paths)
+local Ui = Paths.UI
+local DeviceUtil = require(Paths.Client.Utils.DeviceUtil)
+local CharacterController = require(Paths.Client.Character.CharacterController)
+local Images = require(Paths.Shared.Images.Images)
+local MobileButtonsScreen = Ui.MobileButtons
+local BackgroundFrame = MobileButtonsScreen.Background
+local Button = require(Paths.Client.UI.Elements.Button)
+
+local function getJumpButtonPositionAndSize()
+    if Ui:FindFirstChild("TouchGui") then
+        return Ui.TouchGui.TouchControlFrame.JumpButton.AbsolutePosition, Ui.TouchGui.TouchControlFrame.JumpButton.AbsoluteSize
+    end
+end
+
+local function getMobileButton(icon: string, position: Vector2, size: Vector2): ImageButton
+    local ImageLabel = Instance.new("ImageLabel")
+    ImageLabel.Size = UDim2.fromOffset(size.X, size.Y)
+    ImageLabel.Position = UDim2.fromOffset(position.X, position.Y)
+    ImageLabel.BackgroundTransparency = 1
+    ImageLabel.Image = Images.Icons.MobileButton
+    ImageLabel.ImageTransparency = 0.35
+    ImageLabel.ScaleType = Enum.ScaleType.Fit
+
+    local ImageButton = Instance.new("ImageButton")
+    ImageButton.Position = UDim2.new(0.5, 0, 0.5, 0)
+    ImageButton.AnchorPoint = Vector2.new(0.5, 0.5)
+    ImageButton.Size = UDim2.new(0.6, 0, 0.6, 0)
+    ImageButton.Image = icon
+    ImageButton.ImageTransparency = 0.35
+    ImageButton.BackgroundTransparency = 1
+    ImageButton.ScaleType = Enum.ScaleType.Fit
+    ImageButton.Parent = ImageLabel
+
+    ImageLabel.Parent = BackgroundFrame
+    return Button
+end
+
+if DeviceUtil.isMobile then
+    --sprint button
+    do
+        local position, size = getJumpButtonPositionAndSize()
+        position -= Vector2.new(0, size.X * 0.95)
+        size = Vector2.new(size.X * 0.8, size.Y * 0.8)
+
+        local sprintButton = getMobileButton(Images.Icons.Sprint, position, size)
+
+        local SprintButtonLoaded = Button.new(sprintButton)
+
+        sprintButton.MouseButton1Down:Connect(function()
+            CharacterController.toggleSprint()
+        end)
+    end
+else
+    MobileButtonsScreen:Destroy()
+end
+
+return MobileButtons
