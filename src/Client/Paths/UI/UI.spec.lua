cceb8e00c6e06ec516cfd3d6313ad4e875673650
Joel Broad <38959456+JoelBrd@users.noreply.github.com>
2022-12-20 09:57:08 +0000

    add UI test for detecting non-xbox friendly button properties

--- src/Client/Paths/UI/UI.spec.lua
+++ src/Client/Paths/UI/UI.spec.lua
@@ -5,12 +5,27 @@ local DescendantLooper = require(ReplicatedStorage.Shared.DescendantLooper)
 return function()
     local issues: { string } = {}
 
-    -- ResetOnSpawn = false
+    -- ResetOnSpawn = false, Selectable+Active = false
     DescendantLooper.add(function(descendant)
-        return descendant:IsA("ScreenGui") and true or false
-    end, function(screenGui: ScreenGui)
-        if screenGui.ResetOnSpawn then
-            table.insert(issues, ("%s ResetOnSpawn is enabled - turn it off!"):format(screenGui:GetFullName()))
+        return (descendant:IsA("ScreenGui") or descendant:IsA("TextButton") or descendant:IsA("ImageButton")) and true or false
+    end, function(instance: ScreenGui | TextButton | ImageButton)
+        -- ScreenGui
+        if instance:IsA("ScreenGui") then
+            if instance.ResetOnSpawn then
+                table.insert(issues, ("%s ResetOnSpawn is enabled - turn it off!"):format(instance:GetFullName()))
+            end
+            return
+        end
+
+        -- Button
+        if instance:IsA("TextButton") or instance:IsA("ImageButton") then
+            if not instance.Selectable then
+                table.insert(issues, ("%s Selectable=false, could cause XBOX issues"):format(instance:GetFullName()))
+            end
+            if not instance.Active then
+                table.insert(issues, ("%s Active=false, could cause XBOX issues"):format(instance:GetFullName()))
+            end
+            return
         end
     end, { StarterGui }, true)
 
